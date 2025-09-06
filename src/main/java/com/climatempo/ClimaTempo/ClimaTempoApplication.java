2c0dc0367e1bafe0638043850e3d9c44e40a392a
Léo Meyer <leocantarella@gmail.com>
2025-09-06 10:40:28 -0300

    Estrutura incial de camadas do projeto

---
+++ src/main/java/com/climatempo/ClimaTempo/ClimaTempoApplication.java
@@ -0,0 +1,13 @@
+package com.climatempo.ClimaTempo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimaTempoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimaTempoApplication.class, args);
+	}
+
+}
